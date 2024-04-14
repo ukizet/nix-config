0e7f73ef0ccb3d83274f11f6f2abbd36036a1cde
ukizet <ukikatuki@gmail.com>
2024-04-14 19:50:55 +0300

    trying to organise everything

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -6,58 +6,73 @@
       ./hardware-configuration.nix
     ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    networkmanager.enable = true;
+  };
   
-  networking.networkmanager.enable = true;
-
   # Set your time zone.
   time.timeZone = "Europe/Kyiv";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "uk_UA.UTF-8";
-    LC_IDENTIFICATION = "uk_UA.UTF-8";
-    LC_MEASUREMENT = "uk_UA.UTF-8";
-    LC_MONETARY = "uk_UA.UTF-8";
-    LC_NAME = "uk_UA.UTF-8";
-    LC_NUMERIC = "uk_UA.UTF-8";
-    LC_PAPER = "uk_UA.UTF-8";
-    LC_TELEPHONE = "uk_UA.UTF-8";
-    LC_TIME = "uk_UA.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "uk_UA.UTF-8";
+      LC_IDENTIFICATION = "uk_UA.UTF-8";
+      LC_MEASUREMENT = "uk_UA.UTF-8";
+      LC_MONETARY = "uk_UA.UTF-8";
+      LC_NAME = "uk_UA.UTF-8";
+      LC_NUMERIC = "uk_UA.UTF-8";
+      LC_PAPER = "uk_UA.UTF-8";
+      LC_TELEPHONE = "uk_UA.UTF-8";
+      LC_TIME = "uk_UA.UTF-8";
+    };
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      # Enable the GNOME Desktop Environment.
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+      # Configure keymap in X11
+      xkb = {
+        layout = "us, ru";
+        variant = "";
+      };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us, ru";
-    variant = "";
+      # Enable automatic login for the user.
+      displayManager.autoLogin = {
+        enable = true;
+        user = "sas";
+      };
+
+      # Enable nvidia drivers
+      videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
+    };
+    printing.enable = true;
+    pipewire = {
+    enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    pulse.enable = true;
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  };
 
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
 
   users.users.sas = {
     isNormalUser = true;
@@ -65,67 +80,62 @@
     extraGroups = [ "networkmanager" "wheel" "docker"];
   };
 
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "sas";
-
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+  systemd.services = {
+    "getty@tty1".enable = false;
+    "autovt@tty1".enable = false;
+  };
 
   # What i added starts here.
 
   boot.kernelPackages = pkgs.linuxPackages_6_8;
 
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  
-  nixpkgs.config.nvidia.acceptLicense = true;
-
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+  nixpkgs.config = {
+    allowUnfree = true;
+    nvidia.acceptLicense = true;
   };
 
-  services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
-
-  hardware.nvidia = {
-
-    modesetting.enable = true;
-
-    powerManagement.enable = false;
-
-    powerManagement.finegrained = false;
-
-    open = false;
-
-    nvidiaSettings = true;
-
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  hardware.nvidia.prime = {
-    sync.enable = true;
-
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
+  hardware = {
+    # Enable OpenGL
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement = {
+        enable = false;
+        finegrained = false;
+      };
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      prime = {
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
   };
 
   programs.steam.enable = true;
   
-  virtualisation.docker.enable = true;
-  #virtualisation.docker.storageDriver = "ext4";
-  virtualisation.docker.rootless = {
+  virtualisation.docker = {
   	enable = true;
-  	setSocketVariable = true;
+  	#storageDriver = "ext4";
+  	rootless = {
+			enable = true;
+			setSocketVariable = true;
+		};
   };
   
-  services.flatpak.enable = true;
-  services.flatpak.packages = [
-    "net.xmind.XMind"
-  ];
+  services.flatpak = {
+  	enable = true;
+  	packages = [
+		  "net.xmind.XMind"
+		];
+  };
 
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
@@ -163,15 +173,20 @@
     gamemode
     steamPackages.steamcmd
     godot_4
+    nixpkgs-fmt
   ];
 
   fonts.packages = with pkgs; [
     fira-code-nerdfont
   ];
 
-  nix.optimise.automatic = true;
-  nix.optimise.dates = [ "03:45" ];
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+  	optimise = {
+  		automatic = true;
+  		dates = [ "03:45" ];
+  	};
+  	settings.experimental-features = [ "nix-command" "flakes" ];
+  };
   system.stateVersion = "23.05";
 
 }
