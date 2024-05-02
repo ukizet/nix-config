fc658b12d80e126e8d5b7de8376ea66884dfdf76
ukizet <ukikatuki@gmail.com>
2024-05-02 21:53:35 +0300

    removed docker from systemPakcages

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -148,7 +148,6 @@
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     neofetch
-    docker
     nodejs_20
     python3
     google-chrome
