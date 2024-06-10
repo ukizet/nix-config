d6a7dd20a900130a64a8247b4932bfa6dfb17443
ukizet <ukikatuki@gmail.com>
2024-06-11 01:41:01 +0300

    trying to fix kdenlive

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -63,6 +63,23 @@
 
     flatpak = {
       enable = true;
+      update.auto = {
+				enable = true;
+				onCalendar = "monthly"; # Default value
+			};
+      overrides = {
+				global = {
+				  # Force Wayland by default
+				  Context.sockets = ["wayland" "!x11" "fallback-x11"];
+
+				  Environment = {
+				    # Fix un-themed cursor in some Wayland apps
+				    XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
+
+				    # Force correct theme for some GTK apps
+				    GTK_THEME = "Adwaita:dark";
+				  };
+				};
       packages = [
         "net.xmind.XMind"
         "com.github.tchx84.Flatseal"
