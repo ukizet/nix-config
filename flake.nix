efede9eea662f2bf151b792dba524c9249884209
ukizet <ukikatuki@gmail.com>
2024-06-21 14:21:09 +0300

    changed unstable branch to stable 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My system flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nix-flatpak.url = "github:gmodena/nix-flatpak"; # unstable branch. Use github:gmodena/nix-flatpak/?ref=<tag> to pin releases.
     home-manager = {
       url = "github:nix-community/home-manager";
