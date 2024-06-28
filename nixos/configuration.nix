d84283e8a1a1ff0796ad801b9aa8112fd0d59db2
ukizet <ukikatuki@gmail.com>
2024-06-28 12:13:19 +0300

    changed bottles back to flatpak

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -88,8 +88,9 @@
         "org.qbittorrent.qBittorrent"
         "org.telegram.desktop"
         "com.dec05eba.gpu_screen_recorder"
-        "com.bitwig.BitwigStudio"
         "io.github.giantpinkrobots.flatsweep"
+        "com.usebottles.bottles"
+        "com.hunterwittenborn.Celeste"
       ];
     };
   };
@@ -163,7 +164,6 @@
       bitwarden # password manager
       localsend # files sharing
       bisq-desktop # open source cryptoexchange
-      bottles # wineprefix manager
       # nixos related
       nixpkgs-fmt
       appimage-run # workaround for appimages
@@ -175,6 +175,7 @@
       stremio # movies & anime & shows
       spotube # music
       kdePackages.kdenlive # video editing
+      bitwig-studio
       # related to languages
       nodejs_20
       python3
