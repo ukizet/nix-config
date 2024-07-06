5d7d8d3383ce605e163ee8c255974d7bc69de3d1
Vlad <93866925+ukizet@users.noreply.github.com>
2024-07-06 15:54:12 +0300

    Update newinstall.sh

--- newinstall.sh
+++ newinstall.sh
@@ -1,4 +1,4 @@
-curl "https://raw.githubusercontent.com/ukizet/nix-config/stable(24.05)-for-vm/nixos/disk-config.nix" -o ~/disk-config.nix &&
+curl "https://raw.githubusercontent.com/ukizet/nix-config/stable(24.05)/nixos/disk-config.nix" -o ~/disk-config.nix &&
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ~/disk-config.nix && 
 sudo mount | grep /mnt
 read -n 1 -s -r -p "Press any key to generate config and install nixos..."
