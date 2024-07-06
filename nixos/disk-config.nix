b74309088891959d682c3447e922d690ccfc2550
Vlad <93866925+ukizet@users.noreply.github.com>
2024-07-06 13:26:59 +0300

    Update disk-config.nix

--- nixos/disk-config.nix
+++ nixos/disk-config.nix
@@ -2,7 +2,7 @@
   disko.devices = {
     disk = {
       my-disk = {
-        device = "/dev/sda";
+        device = "/dev/vda";
         type = "disk";
         content = {
           type = "gpt";
