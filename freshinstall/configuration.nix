dab4c1280b1510c66303dda7ba789d39bc9854e8
Vlad <93866925+ukizet@users.noreply.github.com>
2024-07-07 01:31:51 +0300

    changed newinstall.sh

---
+++ freshinstall/configuration.nix
@@ -0,0 +1,55 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
+      ./disk-config.nix
+    ];
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxPackages_6_8;
+  };
+
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    networkmanager.enable = true;
+  };
+
+  time.timeZone = "Europe/Kyiv";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "uk_UA.UTF-8";
+      LC_IDENTIFICATION = "uk_UA.UTF-8";
+      LC_MEASUREMENT = "uk_UA.UTF-8";
+      LC_MONETARY = "uk_UA.UTF-8";
+      LC_NAME = "uk_UA.UTF-8";
+      LC_NUMERIC = "uk_UA.UTF-8";
+      LC_PAPER = "uk_UA.UTF-8";
+      LC_TELEPHONE = "uk_UA.UTF-8";
+      LC_TIME = "uk_UA.UTF-8";
+    };
+  };
+
+  systemd.services = {
+    "getty@tty1".enable = false;
+    "autovt@tty1".enable = false;
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    nvidia.acceptLicense = true;
+  };
+
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+  system.stateVersion = "23.05";
+}
--- newinstall.sh
+++ newinstall.sh
@@ -1,5 +1,12 @@
 curl "https://raw.githubusercontent.com/ukizet/nix-config/stable(24.05)-for-vm/nixos/disk-config.nix" -o ~/disk-config.nix &&
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ~/disk-config.nix && 
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ~/disk-config.nix
 sudo mount | grep /mnt
-read -n 1 -s -r -p "Press any key to generate config and install nixos..."
-sudo nixos-generate-config --root /mnt && sudo nixos-install
+sleep 5
+read -n 1 -s -r -p "Press any key to generate config..."
+
+sudo nixos-generate-config --no-filesystems --root /mnt &&
+cd /mnt/etc/nixos &&
+mv ~/disk-config.nix /mnt/etc/nixos &&
+curl "https://raw.githubusercontent.com/ukizet/nix-config/stable(24.05)-for-vm/freshinstall/configuration.nix" -o /mnt/etc/nixos/configuration.nix
+read -n 1 -s -r -p "Press any key to install nixos..."
+sudo nixos-install && reboot
