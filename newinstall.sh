76afa976d8641c963fa0e9f655c004d54db303b9
Vlad <93866925+ukizet@users.noreply.github.com>
2024-07-07 11:21:41 +0300

    Update newinstall.sh

--- newinstall.sh
+++ newinstall.sh
@@ -1,4 +1,4 @@
-curl "https://raw.githubusercontent.com/ukizet/nix-config/stable(24.05)-for-vm/nixos/disk-config.nix" -o ~/disk-config.nix &&
+curl "https://raw.githubusercontent.com/ukizet/nix-config/stable(24.05)/nixos/disk-config.nix" -o ~/disk-config.nix &&
 echo "Downloaded disk-config.nix..."
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ~/disk-config.nix
 echo "Disk was formatted..."
@@ -9,6 +9,6 @@ read -n 1 -s -r -p "Press any key to generate config..."
 sudo nixos-generate-config --no-filesystems --root /mnt &&
 cd /mnt/etc/nixos &&
 mv ~/disk-config.nix /mnt/etc/nixos &&
-curl "https://raw.githubusercontent.com/ukizet/nix-config/stable(24.05)-for-vm/freshinstall/configuration.nix" -o /mnt/etc/nixos/configuration.nix
+curl "https://raw.githubusercontent.com/ukizet/nix-config/stable(24.05)/freshinstall/configuration.nix" -o /mnt/etc/nixos/configuration.nix
 read -n 1 -s -r -p "Press any key to install nixos..."
 sudo nixos-install && reboot
