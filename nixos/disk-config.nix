5498a88a6e9e6f5fe88a8599cdd2aa507b0acd1b
Vlad <93866925+ukizet@users.noreply.github.com>
2024-09-01 13:37:40 +0300

    Update disk-config.nix

--- nixos/disk-config.nix
+++ nixos/disk-config.nix
@@ -2,43 +2,44 @@
   disko.devices = {
     disk = {
       Patriot = {
-      device = "/dev/sda";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          data = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/mnt/Patriot";
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            data = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/mnt/Patriot";
+              };
             };
           };
         };
       };
-    };
-    Kingston = {
-      device = "/dev/nvme0n1";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            type = "EF00";
-            size = "256M";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+      Kingston = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "256M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
             };
-          };
-          root = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
             };
           };
         };
