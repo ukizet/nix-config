1c2a9a66f3f90b2a78651d11c28f1641088b8820
ukizet <ukikatuki@gmail.com>
2025-04-28 17:18:24 +0300

    added keymaps to nvf

--- nixos/programs.nix
+++ nixos/programs.nix
@@ -20,6 +20,10 @@
         viAlias = false;
         vimAlias = true;
         lsp.enable = true;
+        options = {
+          shiftwidth = 2;
+          tabstop = 2;
+        };
         languages = {
           nix = {
             enable = true;
@@ -31,15 +35,20 @@
           markdown.enable = true;
           ts.enable = true;
         };
-        options = {
-          shiftwidth = 2;
-          tabstop = 2;
-        };
         telescope.enable = true;
         autopairs.nvim-autopairs.enable = true;
         autocomplete.nvim-cmp.enable = true;
         fzf-lua.enable = true;
         filetree.neo-tree.enable = true;
+        keymaps = [
+          {
+            key    = "<leader>e";               
+            mode   = ["n"];                     
+            action = "<cmd>Neotree toggle<CR>"; 
+            desc   = "Toggle Neo-Tree";         
+            silent = true;
+          }
+        ];
       };
     };
     zsh.enable = true;
