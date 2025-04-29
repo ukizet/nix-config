0665c42e4f705dca63536db99aa29fdbcc3d26fa
ukizet <ukikatuki@gmail.com>
2025-04-29 14:38:32 +0300

    added hyprland.log

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -24,13 +24,15 @@ in
     enable = true;
     xwayland.enable = true;
     settings = {
+      "debug:disable_logs" = "false";
       "$mod" = "SUPER";
       "$browser" = "librewolf";
       "$terminal" = "kitty";
       "$runner" = "rofi";
       "$explorer" = "dolphin";
       monitor = [
-        "DP-1, preferred, auto, 1"
+        "DP-1, 1920x1080@60, 0x0, 1"
+        "VGA-1, 1920x1080@60, 0x0, 1"
       ];
       bind =
         [
